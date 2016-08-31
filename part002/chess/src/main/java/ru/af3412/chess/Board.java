421d3f3dc0a29e351a7347e2bcf45c1846be8746
filatov <af3412@yandex.ru>
2016-08-31 21:01:55 +0300

    delete all

--- part002/chess/src/main/java/ru/af3412/chess/Board.java
+++ part002/chess/src/main/java/ru/af3412/chess/Board.java
@@ -6,37 +6,8 @@ package ru.af3412.chess;
  */
 public class Board {
 
-    public Cell[][] board = new Cell[8][8];
+    Figure[][] board = new Figure[8][8];
 
-    public void createBoard() {
-        for (int cVer = 0; cVer < 8; cVer++) {
-            for (int cHor = 0; cHor < 8; cHor++) {
-                board[cVer][cHor] = new Cell(cVer, cHor);
-            }
-        }
-    }
-
-
-    protected void printBoard() {
-        for(int countVertical = 0; countVertical < 8; countVertical++) {
-            for(int countHorizontal = 0; countHorizontal < 8; countHorizontal++){
-
-                if (board[countHorizontal][countVertical].getIsFree() == true){
-                    System.out.print("\tO");
-                }
-                else {
-                    System.out.print("\t" + board[countHorizontal][countVertical].nameFigure);
-                }
-            }
-            System.out.println();
-        }
-        System.out.println();
-    }
-
-    /*public void moveFigure(Cell start, Cell distance) {
-        start.
-
-    }*/
 
 
 }
--- part002/chess/src/main/java/ru/af3412/chess/Figure.java
+++ part002/chess/src/main/java/ru/af3412/chess/Figure.java
@@ -2,27 +2,9 @@ package ru.af3412.chess;
 
 /**
  * @author Filatov Alexander
- * @since 23.08.2016
+ * @since 31.08.2016
  */
 public abstract class Figure {
 
-    /**
-     * Figure color
-     * white color true
-     * black color false
-     */
-    boolean color;
-
-    Cell cell;
-
-    //Board chessBoard;
-
-    public Figure(Cell cell, boolean color) {
-      //  this.chessBoard = chessBoard;
-        this.cell = cell;
-        this.color = color;
-    }
-
-   // abstract Cell[] move(Cell distance);
 
 }
