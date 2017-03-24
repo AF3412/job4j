a99ce6c493223b0841e7b11833c757a9655e48d9
AF3412 <ra4059ud>
2017-03-24 15:45:00 +0300

    First working version TDD module, without Exception.

--- part004/tdd/src/main/java/ru/af3412/tdd/SimpleGenerator.java
+++ part004/tdd/src/main/java/ru/af3412/tdd/SimpleGenerator.java
@@ -5,35 +5,26 @@ import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
 /**
- * @author Filatov Alexander
- * @since 09.03.2017
+ * Created by Филатов on 23.03.2017.
  */
 public class SimpleGenerator implements Template {
 
-    public void testRegex(String str) {
-        // "Hello, ${name}.
-        Pattern pt = Pattern.compile("(\\$)(\\{)((\\w)*?)(\\})");
-        Matcher m = pt.matcher(str);
-        boolean st = m.matches();
-        System.out.println("Regex is: " + st);
-
-        System.out.println(str.replaceAll("", "IAMGROOT"));
-
-        String str2 = new String("Добро пожаловать на ProgLang.su");
-
-        System.out.print("Возвращаемое значение: ");
-        System.out.println(str2.replaceAll("(.*)ProgLang(.*)", "IAMGROOT"));
-
-        System.out.print("Возвращаемое значение: ");
-        System.out.println(str2.replaceAll("ProgLang.su", "сайт посвященный программированию!"));
-
-
-
-    }
+    private final String PATTERN = "\\$\\{(\\w+)(\\})";
 
     @Override
     public String generate(String template, Map<String, String> data) {
-        return null;
+
+        Matcher matcher = Pattern.compile(PATTERN).matcher(template);
+        StringBuffer stringBuffer = new StringBuffer();
+
+        while (matcher.find()) {
+            matcher.appendReplacement(stringBuffer, data.get(matcher.group(1)));
+        }
+        matcher.appendTail(stringBuffer);
+
+        return stringBuffer.toString();
     }
+
+
 
 }
--- part004/tdd/src/test/java/ru/af3412/tdd/SimpleGeneratorTest.java
+++ part004/tdd/src/test/java/ru/af3412/tdd/SimpleGeneratorTest.java
@@ -2,25 +2,92 @@ package ru.af3412.tdd;
 
 import org.junit.Test;
 
+import java.util.HashMap;
+import java.util.Map;
+
+import static org.hamcrest.core.Is.is;
+import static org.junit.Assert.assertThat;
+
 /**
- * @author Filatov Alexander
- * @since 09.03.2017
+ * Created by Филатов on 23.03.2017.
  */
 public class SimpleGeneratorTest {
 
-   /* @Test
-    public void generate() throws Exception {
-
+    /**
+     * When set one key return one value.
+     *
+     * @throws Exception the exception
+     */
+    @Test
+    public void whenSendSNameReturnValueFromMap() throws Exception {
         Template template = new SimpleGenerator();
-
         String text = "Hello, ${name}.";
+        Map<String, String> data = new HashMap<>();
+        data.put("name", "Alex");
+        String checked = "Hello, Alex.";
 
-    }*/
+        String result = template.generate(text, data);
 
-   @Test
-    public void testRegex() {
-       SimpleGenerator sg = new SimpleGenerator();
-       sg.testRegex("${name}");
-   }
+        assertThat(result, is(checked));
+    }
+
+    @Test
+    public void whenSendTwoSNameReturnValueFromMap() throws Exception {
+        Template template = new SimpleGenerator();
+        String text = "Hello, ${name}. I'm ${name2}.";
+        Map<String, String> data = new HashMap<>();
+        data.put("name", "Alex");
+        data.put("name2", "Joe");
+        String checked = "Hello, Alex. I'm Joe.";
+
+        String result = template.generate(text, data);
+
+        assertThat(result, is(checked));
+    }
+
+    @Test
+    public void whenSendTwoSNameAndNameTwoBetweenNameOneReturnValueFromMap() throws Exception {
+        Template template = new SimpleGenerator();
+        String text = "Hello, ${name}. I'm ${name2}. How are you, ${name}?";
+        Map<String, String> data = new HashMap<>();
+        data.put("name", "Alex");
+        data.put("name2", "Joe");
+        String checked = "Hello, Alex. I'm Joe. How are you, Alex?";
+
+        String result = template.generate(text, data);
+
+        assertThat(result, is(checked));
+    }
+
+
+
+    @Test
+    public void whenSendThreeIdenticalValuesAllChanges() throws Exception {
+        Template template = new SimpleGenerator();
+        String text = "AAA, ${sos}, ${sos}, ${sos}! Help me!";
+        Map<String, String> data = new HashMap<>();
+        data.put("sos", "SOS");
+        String checked = "AAA, SOS, SOS, SOS! Help me!";
+
+        String result = template.generate(text, data);
+
+        assertThat(result, is(checked));
+    }
+
+
+    @Test
+    public void whenSendSNameWithoutValuesFromMapReturnException() throws Exception {
+        Template template = new SimpleGenerator();
+        String text = "Hello, ${name}.";
+        Map<String, String> data = new HashMap<>();
+        data.put("subject", "Alex");
+        String checked = "Hello, Alex.";
+
+        String result = template.generate(text, data);
+
+        assertThat(result, is(checked));
+    }
+
+
 
 }
