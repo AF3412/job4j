e29e202abf231272a6f33573ac420a1b59400163
AF3412 <ra4059ud>
2017-03-28 14:03:02 +0300

    Refactoring TDD module.

--- part004/tdd/src/main/java/ru/af3412/tdd/SimpleGenerator.java
+++ part004/tdd/src/main/java/ru/af3412/tdd/SimpleGenerator.java
@@ -18,26 +18,24 @@ public class SimpleGenerator implements Template {
     public String generate(String template, Map<String, String> data) throws TemplateException {
 
         /**
-         * Create regular expression for find.
-         */
-
-        Matcher matcher = Pattern.compile(pattern).matcher(template);
-        StringBuffer stringBuffer = new StringBuffer();
-
-        /**
          * Get all keys and check that used in the string. Else create exception.
          */
         for (String key : data.keySet()) {
-            String tmpValue = new StringBuffer("${").append(key).append("}").toString();
-            if (!template.contains(tmpValue)) {
+            if (!template.contains(new StringBuilder("${").append(key).append("}").toString())) {
                 throw new TemplateException("Excess key");
             }
         }
 
         /**
+         * Create regular expression for find.
+         */
+        Matcher matcher = Pattern.compile(pattern).matcher(template);
+
+        /**
          * Find all keys in string and replace to values from data.
          * If Map (data) does not contains value then create exception.
          */
+        StringBuffer stringBuffer = new StringBuffer();
         while (matcher.find()) {
             if (data.containsKey(matcher.group(1))) {
                 matcher.appendReplacement(stringBuffer, data.get(matcher.group(1)));
@@ -49,6 +47,5 @@ public class SimpleGenerator implements Template {
 
         return stringBuffer.toString();
     }
-
 
 }
