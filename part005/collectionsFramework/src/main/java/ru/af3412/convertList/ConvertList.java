df55ca060bec1a1a71d773d87d6431f61b2cf709
AF3412 <ra4059ud>
2017-04-02 10:51:45 +0300

    Add "How IT Work" class for testing and Experimenting with collections.

---
+++ part005/collectionsFramework/src/main/java/ru/af3412/convertList/ConvertList.java
@@ -0,0 +1,18 @@
+package ru.af3412.convertList;
+
+import java.util.List;
+
+/**
+ * Created by Филатов on 31.03.2017.
+ */
+public class ConvertList {
+
+    public List<Integer> toList (int[][] array) {
+        return null;
+    }
+
+    public int[][] toArray (List<Integer> list, int rows) {
+        return null;
+    }
+
+}
