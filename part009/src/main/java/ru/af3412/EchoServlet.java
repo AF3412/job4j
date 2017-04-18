607bd7ffd75737561289b1e82e9e770047b80bfd
af3412 <@tac.local>
2017-04-18 21:23:22 +0300

    Add part009

---
+++ part009/src/main/java/ru/af3412/EchoServlet.java
@@ -0,0 +1,22 @@
+package ru.af3412;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.io.PrintWriter;
+
+/**
+ * Created by Филатов on 18.04.2017.
+ */
+public class EchoServlet extends HttpServlet {
+
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        resp.setContentType("text/html");
+        PrintWriter writer = new PrintWriter(resp.getOutputStream());
+        writer.append("Hello world1!");
+        writer.flush();
+    }
+}
