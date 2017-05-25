21a4687f9e76ca9cba7d693fe51ba239e5f61916
rret <filas@bk.ru>
2017-05-25 16:18:07 +0300

    After applying this commit create new file "IteratorArray.java" in _1_Iterator

---
+++ part005Pro/_1_Iterator/src/main/java/ru/af3412/iterator/IteratorArray.java
@@ -0,0 +1,20 @@
+package ru.af3412.iterator;
+
+import java.util.Iterator;
+
+/**
+ * Created by Филатов on 25.05.2017.
+ */
+public class IteratorArray implements Iterator {
+
+    @Override
+    public boolean hasNext() {
+        return false;
+    }
+
+    @Override
+    public Object next() {
+        return null;
+    }
+
+}
