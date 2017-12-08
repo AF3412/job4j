55921e35504089a07d56088e2a0568b409ae781c
Alexander Filatov <afil3412@gmail.com>
2017-12-08 20:25:37 +0300

    refactor

--- part005Pro/_2_Generic/src/main/java/ru.af3412.generic/UserStore.java
+++ part005Pro/_2_Generic/src/main/java/ru.af3412.generic/UserStore.java
@@ -2,6 +2,8 @@ package ru.af3412.generic;
 
 /**
  * Created by AF on 22.10.2017.
+ *
+ * @param <User> the type parameter
  */
 public class UserStore<User> extends AbstractStore implements Store {
 
