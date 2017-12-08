31c863afe8559efae3b423c06151ae9dacea9b94
Alexander Filatov <afil3412@gmail.com>
2017-12-08 20:25:53 +0300

    5.3.2. Создать контейнер на базе связанного списка [#159][#31233]

---
+++ part005Pro/_3_List/src/main/java/ru/af3412/list/LinkedListContainer.java
@@ -0,0 +1,220 @@
+package ru.af3412.list;
+
+import java.util.Iterator;
+
+/**
+ * Created by AF on 18.11.2017.
+ */
+public class LinkedListContainer<E> implements Iterable {
+
+    /**
+     * размер свзяного списка
+     */
+    private int size = 0;
+
+    /**
+     * Первый опорный элемент списка.
+     * Значение "element" всегда null.
+     * prev всегда null.
+     * next указывает на первый элемент списка.
+     */
+    private Node<E> firstNode;
+
+    /**
+     * Последний опорный элемент списка.
+     * Значение "element" всегда null.
+     * prev указывает на последний элемент списка.
+     * next всегда null.
+     */
+    private Node<E> lastNode;
+
+    /**
+     * Текущий элемент списка.
+     * Необходим для добавления новых элементов списка.
+     */
+    private Node<E> currentNode;
+
+    /**
+     * Конструктор по-умолчанию. Создает опорные элементы.
+     */
+    public LinkedListContainer() {
+        this.firstNode = new Node<>(null, null, this.lastNode);
+        this.lastNode = new Node<>(null, this.firstNode, null);
+    }
+
+    /**
+     * Возврещает итератор листа.
+     * @return iterator.
+     */
+    @Override
+    public Iterator<E> iterator() {
+        return new Itr();
+    }
+
+    /**
+     * добавляем элемент в конец листа.
+     * @param element
+     */
+    public void add(E element) {
+        if (currentNode == null) {
+            Node<E> node = new Node<>(element, firstNode, lastNode);
+            node.prev.next = node;
+            node.next.prev = node;
+            currentNode = node;
+        } else {
+            Node<E> node = new Node<>(element, currentNode, lastNode);
+            node.prev.next = node;
+            node.next.prev = node;
+            currentNode = node;
+        }
+        size++;
+    }
+
+    /**
+     * Добавляем элемент, вставляя его по индексу.
+     * @param index индекс, куда надо вставить элемент.
+     * @param element сам элемент.
+     * @throws IndexOutOfBoundsException если выходит за пределы листа.
+     */
+    public void add(int index, E element) throws IndexOutOfBoundsException {
+        if (!checkSize(index)) {
+            throw new IndexOutOfBoundsException();
+        }
+        if (this.size == 0) {
+            this.add(element);
+        } else {
+            Node<E> tmpNode = findNode(index);
+            Node<E> newNode = new Node<>(element, tmpNode.prev, tmpNode);
+            tmpNode.prev.next = newNode;
+            tmpNode.prev = newNode;
+            this.size++;
+        }
+    }
+
+    /**
+     * Возвращаем значение из Node по индексу.
+     * @param index номер элемента который надо вернуть.
+     * @return значение из Node.
+     * @throws IndexOutOfBoundsException если выходит за пределы листа/
+     */
+    public E get(int index) throws IndexOutOfBoundsException {
+        if (!checkSize(index)) {
+            throw new IndexOutOfBoundsException();
+        }
+        return findNode(index).element;
+    }
+
+    /**
+     * Удаляем элемент по индексу.
+     * @param index который надо удалить.
+     * @throws IndexOutOfBoundsException если выходит за пределы листа.
+     */
+    public void remove(int index) throws IndexOutOfBoundsException {
+        if (!checkSize(index)) {
+            throw new IndexOutOfBoundsException();
+        }
+        Node<E> tmpNode = findNode(index);
+        tmpNode.prev.next = tmpNode.next;
+        tmpNode.next.prev = tmpNode.prev;
+        tmpNode = null;
+        size--;
+    }
+
+    /**
+     * Удаление элемента по его значению.
+     * @param value искомый элемент.
+     */
+    public void remove(E value) {
+        this.remove(findElement(value));
+    }
+
+    /**
+     * Поиск элемента по значению.
+     * @param value значение.
+     * @return индекс данного элемента.
+     */
+    public int findElement(E value) {
+        int result = -1;
+        Node<E> tmpNode = firstNode.next;
+        for (int i = 0; i < size; i++) {
+            if (tmpNode.element.equals(value)) {
+                result = i;
+                break;
+            }
+        }
+        return result;
+    }
+
+    /**
+     * Ищем Node по индексу.
+     * @param index номер Node.
+     * @return Node по индексу.
+     * @throws IndexOutOfBoundsException может выкинуть исключение, если выходит за пределы листа.
+     */
+    private Node<E> findNode(int index) throws IndexOutOfBoundsException {
+        if (!checkSize(index)) {
+            throw new IndexOutOfBoundsException();
+        }
+        Node<E> result = firstNode.next;
+        for (int i = 0; i < index; i++) {
+            result = result.next;
+        }
+        return result;
+    }
+
+    /**
+     * Проверка на выход за пределы листа.
+     * @param index проверка, что элемент находится в пределах листа.
+     * @return true or false.
+     */
+    private boolean checkSize(int index) {
+        return (index >= 0 && index < this.size);
+    }
+
+    /**
+     * Узел списка. Содержит значение и указатели на следующий и предыдущий элементы.
+     *
+     * @param <T> любое значение.
+     */
+    private class Node<T> {
+        T element;
+        Node<T> next;
+        Node<T> prev;
+
+        Node(T element, Node<T> prev, Node<T> next) {
+            this.element = element;
+            this.prev = prev;
+            this.next = next;
+        }
+    }
+
+    /**
+     * Итератор.
+     */
+    private class Itr implements Iterator<E> {
+        /**
+         * Указатель на элемент.
+         */
+        private int cursor = 0;
+
+        /**
+         * Проверяем не вышли ли мы за пределы листа.
+         * @return
+         */
+        @Override
+        public boolean hasNext() {
+            return cursor != size;
+        }
+
+        /**
+         * Берем текущий элемент и переходим на следующий.
+         * @return возвращаем текущий элемент.
+         */
+        @Override
+        public E next() {
+            E result = findNode(cursor).element;
+            cursor++;
+            return result;
+        }
+    }
+}
---
+++ part005Pro/_3_List/src/test/java/ru/af3412/list/LinkedListContainerTest.java
@@ -0,0 +1,126 @@
+package ru.af3412.list;
+
+import org.junit.Test;
+
+import java.util.Iterator;
+
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.core.Is.is;
+
+/**
+ * Created by AF on 19.11.2017.
+ */
+public class LinkedListContainerTest {
+
+
+    /**
+     * When add one value to list return this value.
+     */
+    @Test
+    public void whenAddOneValueToListReturnThisValue() {
+        LinkedListContainer<Integer> listContainer = new LinkedListContainer<>();
+        listContainer.add(1);
+
+        int result = listContainer.get(0);
+
+        assertThat(result, is(1));
+    }
+
+    /**
+     * When add two value to list method return second value.
+     */
+    @Test
+    public void whenAddTwoValueToListMethodReturnSecondValue() {
+        LinkedListContainer<Integer> listContainer = new LinkedListContainer<>();
+        listContainer.add(1);
+        listContainer.add(2);
+
+        int result = listContainer.get(1);
+
+        assertThat(result, is(2));
+    }
+
+    /**
+     * When add value to list between two elements method return this element.
+     */
+    @Test
+    public void whenAddValueToListBetweenTwoElementsMethodReturnThisElement() {
+        LinkedListContainer<Integer> listContainer = new LinkedListContainer<>();
+        listContainer.add(1);
+        listContainer.add(2);
+        listContainer.add(0, 0);
+
+        int result0 = listContainer.get(0);
+        int result1 = listContainer.get(1);
+        int result2 = listContainer.get(2);
+
+        assertThat(result0, is(0));
+        assertThat(result1, is(1));
+        assertThat(result2, is(2));
+    }
+
+    /**
+     * When get element from out of index then throw exception.
+     */
+    @Test(expected = IndexOutOfBoundsException.class)
+    public void whenGetElementFromOutOfIndexThenThrowException() {
+        LinkedListContainer<Integer> listContainer = new LinkedListContainer<>();
+        listContainer.get(1);
+    }
+
+    /**
+     * When delete element then list remove this element.
+     */
+    @Test
+    public void whenDeleteElementByIndexThenListRemoveThisElement() {
+        LinkedListContainer<Integer> listContainer = new LinkedListContainer<>();
+        listContainer.add(1);
+        listContainer.add(2);
+
+        listContainer.remove(0);
+
+        int result = listContainer.get(0);
+
+        assertThat(result, is(2));
+    }
+
+    /**
+     * When delete element by value then list remove this element.
+     */
+    @Test
+    public void whenDeleteElementByValueThenListRemoveThisElement() {
+        LinkedListContainer<String> listContainer = new LinkedListContainer<>();
+        listContainer.add("1");
+        listContainer.add("2");
+
+        listContainer.remove("1");
+
+        String result = listContainer.get(0);
+
+        assertThat(result, is("2"));
+    }
+
+    /**
+     * When create list then iterator method worked.
+     */
+    @Test
+    public void whenCreateListThenIteratorMethodWorked() {
+        LinkedListContainer<Integer> listContainer = new LinkedListContainer<>();
+        listContainer.add(1);
+        listContainer.add(2);
+        listContainer.add(3);
+
+        Iterator iterator = listContainer.iterator();
+        int result;
+        int i = 0;
+
+        while (iterator.hasNext()) {
+            i++;
+            result = (int) iterator.next();
+            assertThat(result, is(i));
+        }
+
+    }
+
+
+}
