a95f89ef48c18165180be401244da4cced1dd291
Alexander Filatov <afil3412@gmail.com>
2018-04-02 21:25:38 +0300

    2. Не перекрывать equals hashCode [#1005]

---
+++ part005Pro/5_Map/src/main/java/ru/af3412/map/package-info.java
@@ -0,0 +1,4 @@
+/**
+ * Created by AF on 02.04.2018.
+ */
+package ru.af3412.map;
---
+++ part005Pro/5_Map/src/test/java/ru/af3412/map/UserTest.java
@@ -0,0 +1,33 @@
+package ru.af3412.map;
+
+import org.junit.Test;
+
+import java.util.*;
+
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.core.Is.is;
+import static org.junit.Assert.*;
+
+/**
+ * Created by AF on 02.04.2018.
+ */
+public class UserTest {
+
+    /**
+     * Test calendar.
+     */
+    @Test
+    public void testCalendar() {
+        Calendar calendar = new GregorianCalendar(2018, 4, 2);
+        User userOne = new User("Alex", 2, calendar);
+        User userTwo = new User("Alex", 2, calendar);
+        Map<User, Object> testMap = new HashMap<>();
+        testMap.put(userOne, new Object());
+        testMap.put(userTwo, new Object());
+
+        System.out.println(testMap);
+    }
+
+
+
+}
