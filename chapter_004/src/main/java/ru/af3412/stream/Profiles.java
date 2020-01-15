b81630d0e17158778c316d3a0cfb2c95490bb1b6
Filatov <alex.filatov@sparox.ru>
2020-01-15 10:56:53 +0300

    3. Уникальность элементов и сортировка. [#110225]

--- chapter_004/src/main/java/ru/af3412/stream/Profiles.java
+++ chapter_004/src/main/java/ru/af3412/stream/Profiles.java
@@ -1,5 +1,6 @@
 package ru.af3412.stream;
 
+import java.util.Comparator;
 import java.util.List;
 import java.util.stream.Collectors;
 
@@ -9,6 +10,15 @@ public class Profiles {
         return profiles
                 .stream()
                 .map(Profile::getAddress)
+                .collect(Collectors.toList());
+    }
+
+    List<Address> collectDistinctSorted(List<Profile> profiles) {
+        return profiles
+                .stream()
+                .map(Profile::getAddress)
+                .sorted(Comparator.comparing(Address::getCity))
+                .distinct()
                 .collect(Collectors.toList());
     }
 
--- chapter_004/src/test/java/ru/af3412/stream/ProfilesTest.java
+++ chapter_004/src/test/java/ru/af3412/stream/ProfilesTest.java
@@ -10,15 +10,11 @@ import static org.junit.Assert.assertThat;
 
 public class ProfilesTest {
 
-    private final Profile firstClient = new Profile(new Address("Kazan", "Baumana", 1, 5));
+    private final Profile firstClient = new Profile(new Address("Moscow", "Tverskaya", 1, 5));
     private final Profile secondClient = new Profile(new Address("Kazan", "Pravo-Bulachnaya", 5, 25));
-    private final Profile thirdClient = new Profile(new Address("Kazan", "Pushkina", 7, 7));
-
-    private final List<Profile> clientProfiles = new ArrayList<>() { {
-        add(firstClient);
-        add(secondClient);
-        add(thirdClient);
-    } };
+    private final Profile thirdClient = new Profile(new Address("Ufa", "Pushkina", 7, 7));
+    private final Profile fourClient = new Profile(new Address("Ufa", "Pushkina", 7, 7));
+    private final Profile fiveClient = new Profile(new Address("Moscow", "Tverskaya", 1, 5));
 
 
     /*
@@ -26,14 +22,44 @@ public class ProfilesTest {
     */
     @Test
     public void whenCollectClientsAddressThenReturnListAddress() {
-
+        final List<Profile> clientProfiles = new ArrayList<>() { {
+            add(firstClient);
+            add(secondClient);
+            add(thirdClient);
+        } };
         Profiles profiles = new Profiles();
         List<Address> result = profiles.collect(clientProfiles);
 
         List<Address> expected = new ArrayList<>() { {
-            add(new Address("Kazan", "Baumana", 1, 5));
+            add(new Address("Moscow", "Tverskaya", 1, 5));
             add(new Address("Kazan", "Pravo-Bulachnaya", 5, 25));
-            add(new Address("Kazan", "Pushkina", 7, 7));
+            add(new Address("Ufa", "Pushkina", 7, 7));
+        } };
+
+        assertThat(result, is(expected));
+
+    }
+
+    /*
+    1. Нужно в списке оставить только уникальные элементы.
+    2. Для быстрого поиска по адресам список нужно было отсортировать.
+    */
+    @Test
+    public void whenCollectClientsAddressThenReturnDistinctSortedListAddress() {
+        final List<Profile> clientProfiles = new ArrayList<>() { {
+            add(secondClient);
+            add(thirdClient);
+            add(firstClient);
+            add(fiveClient);
+            add(fourClient);
+        } };
+        Profiles profiles = new Profiles();
+        List<Address> result = profiles.collectDistinctSorted(clientProfiles);
+
+        List<Address> expected = new ArrayList<>() { {
+            add(new Address("Kazan", "Pravo-Bulachnaya", 5, 25));
+            add(new Address("Moscow", "Tverskaya", 1, 5));
+            add(new Address("Ufa", "Pushkina", 7, 7));
         } };
 
         assertThat(result, is(expected));
