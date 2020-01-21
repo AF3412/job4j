08d73114264d4a3c828d1414f2e98be7f6b7e238
Filatov <alex.filatov@sparox.ru>
2020-01-21 08:34:24 +0300

    Collection API Улучшения [#70623]

--- chapter_004/src/test/java/ru/af3412/stream/SchoolTest.java
+++ chapter_004/src/test/java/ru/af3412/stream/SchoolTest.java
@@ -3,7 +3,6 @@ package ru.af3412.stream;
 import org.junit.Test;
 
 import java.util.ArrayList;
-import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
@@ -14,30 +13,30 @@ public class SchoolTest {
 
     private final School school = new School();
 
-    private final List<Student> students = new ArrayList<>() { {
-        add(new Student(10));
-        add(new Student(20));
-        add(new Student(30));
-        add(new Student(40));
-        add(new Student(50));
-        add(new Student(60));
-        add(new Student(70));
-        add(new Student(80));
-        add(new Student(90));
-        add(new Student(100));
-    } };
+    private final List<Student> students = List.of(
+            new Student(10),
+            new Student(20),
+            new Student(30),
+            new Student(40),
+            new Student(50),
+            new Student(60),
+            new Student(70),
+            new Student(80),
+            new Student(90),
+            new Student(100)
+    );
 
     /*
         10А диапазон балла [70: 100];
     */
     @Test
     public void whenFilterFor10AGroupThenClassWithScoreBetween70And100() {
-        List<Student> expected = new ArrayList<>() { {
-            add(new Student(70));
-            add(new Student(80));
-            add(new Student(90));
-            add(new Student(100));
-        } };
+        List<Student> expected = List.of(
+                new Student(70),
+                new Student(80),
+                new Student(90),
+                new Student(100)
+        );
         List<Student> result = school
                 .collect(students, (student) -> (student.getScore() >= 70 && student.getScore() <= 100));
         assertThat(result, is(expected));
@@ -48,10 +47,10 @@ public class SchoolTest {
     */
     @Test
     public void whenFilterFor10BGroupThenClassWithScoreBetween50And70() {
-        List<Student> expected = new ArrayList<>() { {
-            add(new Student(50));
-            add(new Student(60));
-        } };
+        List<Student> expected = List.of(
+                new Student(50),
+                new Student(60)
+        );
         List<Student> result = school
                 .collect(students, (student) -> (student.getScore() >= 50 && student.getScore() < 70));
         assertThat(result, is(expected));
@@ -62,12 +61,12 @@ public class SchoolTest {
     */
     @Test
     public void whenFilterFor10CGroupThenClassWithScoreBetween0And50() {
-        List<Student> expected = new ArrayList<>() { {
-            add(new Student(10));
-            add(new Student(20));
-            add(new Student(30));
-            add(new Student(40));
-        } };
+        List<Student> expected = List.of(
+                new Student(10),
+                new Student(20),
+                new Student(30),
+                new Student(40)
+        );
         List<Student> result = school
                 .collect(students, (student) -> (student.getScore() >= 0 && student.getScore() < 50));
         assertThat(result, is(expected));
@@ -75,16 +74,17 @@ public class SchoolTest {
 
     @Test
     public void whenCollectStudentsToMapThenReturnStudentsMap() {
-        final List<Student> students = new ArrayList<>() { {
-            add(new Student(10, "Иванов"));
-            add(new Student(20, "Петров"));
-            add(new Student(30, "Сидоров"));
-        } };
+        final List<Student> students = List.of(
+                new Student(10, "Иванов"),
+                new Student(20, "Петров"),
+                new Student(30, "Сидоров")
+        );
 
-        final Map<String, Student> expected = new HashMap<>();
-        expected.put("Иванов", new Student(10, "Иванов"));
-        expected.put("Петров", new Student(20, "Петров"));
-        expected.put("Сидоров", new Student(30, "Сидоров"));
+        final Map<String, Student> expected = Map.of(
+                "Иванов", new Student(10, "Иванов"),
+                "Петров", new Student(20, "Петров"),
+                "Сидоров", new Student(30, "Сидоров")
+        );
 
         final Map<String, Student> result = school.collectMap(students);
 
@@ -94,17 +94,18 @@ public class SchoolTest {
 
     @Test
     public void whenCollectStudentsWithDuplicatesToMapThenReturnStudentsMap() {
-        final List<Student> students = new ArrayList<>() { {
-            add(new Student(10, "Иванов"));
-            add(new Student(10, "Иванов"));
-            add(new Student(20, "Петров"));
-            add(new Student(30, "Сидоров"));
-        } };
+        final List<Student> students = List.of(
+                new Student(10, "Иванов"),
+                new Student(10, "Иванов"),
+                new Student(20, "Петров"),
+                new Student(30, "Сидоров")
+        );
 
-        final Map<String, Student> expected = new HashMap<>();
-        expected.put("Иванов", new Student(10, "Иванов"));
-        expected.put("Петров", new Student(20, "Петров"));
-        expected.put("Сидоров", new Student(30, "Сидоров"));
+        final Map<String, Student> expected = Map.of(
+                "Иванов", new Student(10, "Иванов"),
+                "Петров", new Student(20, "Петров"),
+                "Сидоров", new Student(30, "Сидоров")
+        );
 
         final Map<String, Student> result = school.collectMap(students);
 
@@ -114,18 +115,18 @@ public class SchoolTest {
 
     @Test
     public void whenCollectStudentsListWithNullAndBoundMoreScoreThenReturnSortedListStudents() {
-        final List<Student> students = new ArrayList<>() { {
-            add(new Student(20, "Петров"));
-            add(null);
-            add(new Student(10, "Иванов"));
-            add(new Student(30, "Сидоров"));
-            add(null);
+        List<Student> students = new ArrayList<>() { {
+                add(new Student(20, "Петров"));
+                add(null);
+                add(new Student(10, "Иванов"));
+                add(new Student(30, "Сидоров"));
+                add(null);
         } };
         final int bound = 10;
-        final List<Student> expected = new ArrayList<>() { {
-            add(new Student(30, "Сидоров"));
-            add(new Student(20, "Петров"));
-        } };
+        final List<Student> expected = List.of(
+                new Student(30, "Сидоров"),
+                new Student(20, "Петров")
+        );
 
         final List<Student> result = school.levelOf(students, bound);
 
