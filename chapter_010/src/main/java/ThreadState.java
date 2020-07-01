11d9fd77e1617cdfdbac2ef816bd3d59c2095fc8
Alexander Filatov <alex.filatov@sparox.ru>
2020-07-01 18:30:08 +0400

    1.1. Состояние нити. [#229175]

---
+++ chapter_010/src/main/java/ThreadState.java
@@ -0,0 +1,24 @@
+public class ThreadState {
+    public static void main(String[] args) {
+        Thread first = new Thread(
+                () -> {}
+        );
+        Thread second = new Thread(
+                () -> {}
+        );
+        System.out.println("first: " + first.getName() + " second: " + second.getName());
+        System.out.println("Threads first is: " + first.getState() + " Threads second is: " + second.getState());
+        first.start();
+        second.start();
+        while (first.getState() != Thread.State.TERMINATED) {
+            System.out.println("Threads first is " + first.getState());
+        }
+        while (second.getState() != Thread.State.TERMINATED) {
+            System.out.println("Threads second is " + second.getState());
+        }
+
+        System.out.println("All threads is TERMINATED");
+        System.out.println("Threads first is: " + first.getState() + " Threads second is: " + second.getState());
+        System.out.println("Thread main is: " + Thread.currentThread().getState());
+    }
+}
