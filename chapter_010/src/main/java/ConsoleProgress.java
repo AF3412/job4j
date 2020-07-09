52ad0944f0fa8401aa472f0ae6ec3dc7e2dbf4f6
Alexander Filatov <alex.filatov@sparox.ru>
2020-07-09 11:50:51 +0400

    3. Прерывание нити [#1019]

---
+++ chapter_010/src/main/java/ConsoleProgress.java
@@ -0,0 +1,38 @@
+public class ConsoleProgress implements Runnable {
+
+    @Override
+    public void run() {
+        String twistVal = "-";
+        while (!Thread.currentThread().isInterrupted()) {
+            System.out.print("\r" + twistVal);
+            twistVal = twist(twistVal);
+            try {
+                Thread.sleep(500);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+                break;
+            }
+        }
+    }
+
+    public static void main(String[] args) throws InterruptedException {
+        Thread progress = new Thread(new ConsoleProgress());
+        progress.start();
+        Thread.sleep(10000);
+        progress.interrupt();
+    }
+
+    private String twist(String value) {
+        switch (value) {
+            case "-":
+                return "\\";
+            case "\\":
+                return "|";
+            case "|":
+                return "/";
+            default:
+                return "-";
+        }
+    }
+
+}
