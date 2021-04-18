a44003991859499f09aad116c13682d1d0c228ab
alex.filatov <alex.filatov@sparox.ru>
2021-04-18 15:54:23 +0300

    Dependency injection example

---
+++ chapter_011/src/main/java/ru/af3412/di/Main.java
@@ -0,0 +1,15 @@
+package ru.af3412.di;
+
+public class Main {
+    public static void main(String[] args) {
+        Context context = new Context();
+        context.reg(Store.class);
+        context.reg(StartUI.class);
+
+        StartUI ui = context.get(StartUI.class);
+
+        ui.add("Petr Arsentev");
+        ui.add("Ivan ivanov");
+        ui.print();
+    }
+}
